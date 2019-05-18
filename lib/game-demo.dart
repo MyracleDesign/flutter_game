597e6351a7e714d43c85131d7e9470ada19bfd64
Max Weber <max.weber.consultant@outlook.com>
2019-05-18 17:09:08 +1200

    make it possible to squash some flies

---
+++ lib/game-demo.dart
@@ -0,0 +1,64 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/gestures.dart';
+
+import 'components/fly.dart';
+
+class GameDemo extends Game {
+  Size screenSize;
+  double tileSize;
+  List<Fly> flies;
+  Random rnd;
+
+  GameDemo() {
+    initialize();
+  }
+
+  @override
+  void render(Canvas canvas) {
+    // TODO: implement render
+    Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
+    Paint bgPaint = Paint();
+    bgPaint.color = Color(0xff576574);
+    canvas.drawRect(bgRect, bgPaint);
+
+    flies.forEach((Fly fly) => fly.render(canvas));
+  }
+
+  @override
+  void update(double t) {
+    // TODO: implement update
+    flies.forEach((Fly fly) => fly.update(t));
+    flies.removeWhere((Fly fly) => fly.isOffScreen);
+  }
+
+  void resize(Size size) {
+    screenSize = size;
+    tileSize = screenSize.width / 9;
+  }
+
+  void onTabDown(TapDownDetails d) {
+    flies.forEach((Fly fly) {
+      if (fly.flyRect.contains(d.globalPosition)) {
+        fly.onTapDown();
+      }
+    });
+  }
+
+  void initialize() async {
+    flies = new List<Fly>();
+    rnd = Random();
+    resize(await Flame.util.initialDimensions());
+
+    spawnFly();
+  }
+
+  void spawnFly() {
+    double x = rnd.nextDouble() * (screenSize.width - tileSize);
+    double y = rnd.nextDouble() * (screenSize.height - tileSize);
+    flies.add(Fly(this, x, y));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/util.dart';
 import 'package:flutter/services.dart';
+
+import 'game-demo.dart';
 
 void main() async {
   Util flameUtil = Util();
   await flameUtil.fullScreen();
   await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+  GameDemo game = new GameDemo();
+  runApp(game.widget);
+
+  TapGestureRecognizer tapper = TapGestureRecognizer();
+  tapper.onTapDown = game.onTabDown;
+  flameUtil.addGestureRecognizer(tapper);
 }
